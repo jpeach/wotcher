ffe72db1769fefc7be7c575c1797d6c3747a0045
James Peach <jpeach@apache.org>
2021-03-01 07:16:18 +1100

    Parse argv into GKV for setting up informers.

--- pkg/cli/watch.go
+++ pkg/cli/watch.go
@@ -1,14 +1,94 @@
 package cli
 
-import "github.com/spf13/cobra"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jpeach/wotcher/pkg/k"
+
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
+)
+
+const Progname = "wotcher"
+
+func BuildResourceMapping(r *k.Reader) (map[string][]schema.GroupVersionKind, error) {
+	resourceMap := map[string][]schema.GroupVersionKind{}
+
+	_, resources, err := r.Discovery.ServerGroupsAndResources()
+	if err != nil {
+		return nil, fmt.Errorf("failed to query API resources: %w", err)
+	}
+
+	for _, r := range resources {
+		gv, err := schema.ParseGroupVersion(r.GroupVersion)
+		if err != nil {
+			return nil, err
+		}
+
+		if _, ok := resourceMap[gv.String()]; !ok {
+			resourceMap[gv.String()] = []schema.GroupVersionKind{}
+		}
+
+		if _, ok := resourceMap[gv.Group]; !ok {
+			resourceMap[gv.Group] = []schema.GroupVersionKind{}
+		}
+
+		for _, k := range r.APIResources {
+			if _, ok := resourceMap[k.Kind]; !ok {
+				resourceMap[k.Kind] = []schema.GroupVersionKind{}
+			}
+
+			gvk := gv.WithKind(k.Kind)
+
+			resourceMap[k.Kind] = append(resourceMap[k.Kind], gvk)
+			resourceMap[gv.String()] = append(resourceMap[gv.String()], gvk)
+			resourceMap[gv.Group] = append(resourceMap[gv.Group], gvk)
+		}
+	}
+
+	return resourceMap, nil
+}
+
+func WatchMatchingResources(r *k.Reader, matches []string) error {
+	resourceMap, _ := BuildResourceMapping(r)
+
+	for _, a := range matches {
+		resources, ok := resourceMap[a]
+		if !ok || len(resources) == 0 {
+			fmt.Fprintf(os.Stderr, "%s: no API group or kind match for %q\n", Progname, a)
+			continue
+		}
+
+		for _, gvk := range resources {
+			fmt.Printf("%s: informing on '%s'\n", Progname, gvk)
+			_, _ = r.Cache.GetInformerForKind(context.Background(), gvk)
+		}
+	}
+
+	return nil
+}
 
 // NewWatcher ...
 func NewWatcher() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "wotcher",
+		Use:   Progname,
 		Short: "Watch stuff change in Kubernetes",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			return nil
+			s := k.NewScheme(capi.AddToScheme)
+
+			r, err := k.NewReaderForScheme(s)
+			if err != nil {
+				return err
+			}
+
+			if err := WatchMatchingResources(r, args); err != nil {
+				return err
+			}
+
+			return r.Cache.Start(context.Background())
 		},
 	}
 
--- pkg/k/util.go
+++ pkg/k/util.go
@@ -1,13 +1,26 @@
 package k
 
 import (
+	"k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/discovery"
 	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/restmapper"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 )
+
+// Reader provides read-only access to Kubernetes resources.
+type Reader struct {
+	Conf      *rest.Config
+	Cache     cache.Cache
+	Mapper    meta.RESTMapper
+	Discovery *discovery.DiscoveryClient
+}
 
 // NewScheme ...
 func NewScheme(addTo ...func(*runtime.Scheme) error) *runtime.Scheme {
@@ -43,23 +56,37 @@ func NewClientForScheme(s *runtime.Scheme) (client.Client, error) {
 	)
 }
 
-// NewCacheForScheme returns a new informer cache that uses the given scheme.
-func NewCacheForScheme(s *runtime.Scheme) (cache.Cache, error) {
-	conf, err := ctrl.GetConfig()
+// NewReaderForScheme returns a new informer cache that uses the given scheme.
+func NewReaderForScheme(s *runtime.Scheme) (*Reader, error) {
+	var err error
+	var reader Reader
+
+	reader.Conf, err = ctrl.GetConfig()
 	if err != nil {
 		return nil, err
 	}
 
-	mapper, err := apiutil.NewDiscoveryRESTMapper(conf)
+	reader.Discovery, err = discovery.NewDiscoveryClientForConfig(reader.Conf)
 	if err != nil {
 		return nil, err
 	}
 
-	return cache.New(conf,
+	gr, err := restmapper.GetAPIGroupResources(reader.Discovery)
+	if err != nil {
+		return nil, err
+	}
+
+	reader.Mapper = restmapper.NewDiscoveryRESTMapper(gr)
+
+	reader.Cache, err = cache.New(reader.Conf,
 		cache.Options{
-			Scheme: s,
-			Mapper: mapper,
-		},
-	)
+			Scheme:    s,
+			Mapper:    reader.Mapper,
+			Namespace: metav1.NamespaceAll,
+		})
+	if err != nil {
+		return nil, err
+	}
 
+	return &reader, nil
 }
